a84f30c2b3582b2bfd26901f2b2ffe03a5b1ea33
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-11 18:20:16 -0600

    new commit

--- run_plot.sh
+++ run_plot.sh
@@ -1,38 +1,22 @@
 #! /bin/bash
 
-# epsilonarray=(0.1 0.05 0.01 0.005) #Computation of coarse grid and psi10.5
-# fractionarray=(0.1 0.05 0.01 0.005)
-epsilonarray=(0.1) #Computation of coarse grid and psi10.5
+epsilonarray=(0.1) 
 fractionarray=(0.1)
-
-# epsilonarray=(0.01) #Computation of coarse grid and psi10.5
-# fractionarray=(0.01)
-
-
-# epsilonarray=(0.5) #Computation of coarse grid and psi10.5
-# fractionarray=(0.5)
-
 actiontime=1
 
 python_name="plot.py"
 
 maxiter=400000
 
-# rhoarray=(0.667 0.750 1.00001 1.333 1.500)
-# rhoarray=(0.800 0.850 0.900 0.950 1.00001 1.050 1.100 1.150 1.200 1.250 1.300)
-# rhoarray=(0.800 0.850 0.900)
-# rhoarray=(0.900 0.950 1.00001 1.050 1.100 1.150 1.200 1.250 1.300)
-rhoarray=(0.800 0.850 0.900 0.950 1.00001 1.050 1.100 1.150 1.200 1.250 1.300 1.350 1.400 1.450 1.500)
-# ellarray=(-5.0 -4.0 -3.0 -2.0 -1.0 0.00001 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0)
-# ellarray=(1.00 1.10 1.20 1.30 1.40 1.50 1.60 1.70 1.80 1.90 2.00 2.10 2.20 2.30 2.40)
-ellarray=(0.1111111111111111)
+rhoarray=(0.650 0.700 0.750 0.800 0.850 0.900 0.950 1.00001 1.050 1.100 1.150 1.200 1.250 1.300 1.350 1.400 1.450 1.500)
+ellarray=(0.111)
 for epsilon in ${epsilonarray[@]}; do
     for fraction in "${fractionarray[@]}"; do
         for rho in "${rhoarray[@]}"; do
             for ell in "${ellarray[@]}"; do
                 count=0
 
-                action_name="OneCapital_newcab_newgrid_morepts_opt2"
+                action_name="OneCapital_small_grid"
 
                 dataname="${action_name}_${epsilon}_frac_${fraction}"
                 
@@ -82,7 +66,6 @@ eval "echo Elapsed time: \$(date -ud "@\$elapsed" +'\$((%s/3600/24)) days %H hr 
 EOF
                 count=$(($count + 1))
                 sbatch ./bash/${action_name}/p_eps_${epsilon}_frac_${fraction}/rho_${rho}_ell_${ell}.sh
-            #                 done
             done
         done
     done
