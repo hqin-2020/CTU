d5a6708e87c62714d09d4d9fec4c95038c8be906
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-22 22:07:18 -0600

    new commit

--- twocap3d.py
+++ twocap3d.py
@@ -147,9 +147,9 @@ while FC_Err > tol and epoch < max_iter:
     i1_star[i1_star>=alpha] = alpha-0.001
     i2_star[i2_star>=alpha] = alpha-0.001
 
-    c= alpha - i1_star*k1a - i2_star*k2a
+    c_star= alpha - i1_star*k1a - i2_star*k2a
     
-    mc = delta * np.exp((rho-1)*V0) * c**(-rho)
+    mc = delta * np.exp((rho-1)*V0) * c_star**(-rho)
     
     i1_new = ((1-zeta)*k1a**(1-kappa) - dVdW1) / (mc*k1a) - 1
     i1_new = i1_new/phi1
@@ -160,10 +160,10 @@ while FC_Err > tol and epoch < max_iter:
     i1 = i1_new * fraction + i1_star*(1-fraction)
     i2 = i2_new * fraction + i2_star*(1-fraction)
 
-    i1[i1>=alpha] = alpha-0.001
-    i2[i2>=alpha] = alpha-0.001
+    i1[i1>=alpha] = alpha*4/5-0.001
+    i2[i2>=alpha] = alpha*4/5-0.001
 
-    # c = alpha - i1*k1a - i2*k2a
+    c = alpha - i1*k1a - i2*k2a
 
 ########################## distortion #############
 
