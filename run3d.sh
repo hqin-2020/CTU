030dca6446e9cadfb287cd966524f5a7f63be50c
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-24 10:24:02 -0600

    new commit

--- run3d.sh
+++ run3d.sh
@@ -30,7 +30,7 @@ for epsilon in ${epsilonarray[@]}; do
                         count=0
 
                         # action_name="TwoCapital_small_grid_Acap_smallerepsilon"
-                        action_name="newtestpe"
+                        action_name="oldtestpe"
 
                         dataname="${action_name}_${epsilon}_frac_${fraction}"
 
