48ebf2677686211b457e4bb2bbd68c099ffed6d7
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-24 18:06:31 -0600

    new commit

--- twocap3d.py
+++ twocap3d.py
@@ -239,7 +239,7 @@ while FC_Err > tol and epoch < max_iter:
     C_23_1d = C_23.ravel(order='F')
     C_31_1d = C_31.ravel(order='F')
     D_1d = D.ravel(order='F')
-    petsclinearsystemXDiff.formLinearSystem_DirectCrossDiff_Mix(W1_mat_1d, W2_mat_1d, W3_mat_1d, A_1d, B_1_1d, B_2_1d,
+    petsclinearsystemXDiff.formLinearSystem_DirectCrossDiff_Neumann(W1_mat_1d, W2_mat_1d, W3_mat_1d, A_1d, B_1_1d, B_2_1d,
                                        B_3_1d, C_1_1d, C_2_1d, C_3_1d, C_12_1d, C_23_1d, C_31_1d, epsilon, lowerLims, upperLims, dVec, increVec, petsc_mat)
     V0_1d = V0.ravel(order='F')
     # b = V0_1d / epsilon + D_1d 
